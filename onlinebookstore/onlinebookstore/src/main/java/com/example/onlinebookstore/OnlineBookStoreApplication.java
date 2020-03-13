d64bcc7bad9ad1d19da0f07a07f0c62fda8613d5
Slavko <Slavko@Slavko-PC>
2020-03-13 12:25:18 +0100

    Completed the backend for feature one.

--- onlinebookstore/onlinebookstore/src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/onlinebookstore/src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,10 +1,14 @@
 package com.example.onlinebookstore;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStoreApplication.class, args);
